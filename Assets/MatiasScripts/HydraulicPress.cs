91c16f1182f85d6017f1a55af7f010c1096c7aea
MatiasMC1 <matis123457@hotmail.com>
2025-05-26 13:38:24 +0200

    Hydraulic Press
    Hydraulic Press made, need death scene and sprite for it.

---
+++ Assets/MatiasScripts/HydraulicPress.cs
@@ -0,0 +1,33 @@
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class HydraulicPress : MonoBehaviour
+{
+    public Collider2D deathCollider;
+    public GameObject hydraulicPress;
+
+    public float speed; 
+
+    private void FixedUpdate()
+    {
+        HydraulicMovement();
+    }
+    
+    void HydraulicMovement()
+    {
+        float y = Mathf.PingPong(Time.time * speed, 1) * 6 - 1;
+        hydraulicPress.transform.position = new Vector3(0, y, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(deathCollider)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                //LoadScene("loseMenu")
+            }
+        }
+    }
+}
