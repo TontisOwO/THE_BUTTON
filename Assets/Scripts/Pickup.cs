d7b6fb5372ea4ca2914d7744a097e5e2967a6249
Azure Biander <azure.biander@learnet.se>
2025-05-27 18:02:57 +0200

    Held mirror works

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -7,12 +7,13 @@ public class Pickup : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.CompareTag("HeldMirror"))
+        if (collision.transform.CompareTag("Mirror") && collision.gameObject.layer == 2)
         {
             collision.transform.GetComponent<BoxCollider2D>().isTrigger = true;
             rotation = collision.transform.rotation;
             collision.transform.SetParent(transform);
             collision.transform.SetPositionAndRotation(height + transform.position, rotation);
+            
         }
     }
 }
