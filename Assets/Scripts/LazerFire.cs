9be4ba9460eb539396f8e08df765ed7b3e48561f
Azure Biander <azure.biander@learnet.se>
2025-05-27 12:26:58 +0200

    Lazer now looks decent

--- Assets/Scripts/LazerFire.cs
+++ Assets/Scripts/LazerFire.cs
@@ -5,64 +5,82 @@ public class LazerFire : MonoBehaviour
 {
     [SerializeField] int reflections;
     [SerializeField] float maxRayLength;
-    [SerializeField] LineRenderer myLineRenderer;
+    [SerializeField] bool isActive;
     [SerializeField] Ray ray;
     [SerializeField] RaycastHit hit;
     [SerializeField] Vector3 direction;
+    [SerializeField] LayerMask ignoreLayer;
+    [SerializeField] Material myMaterial;
     [SerializeField] Transform targetHitTransform;
     [SerializeField] Transform[] newMirrors;
     [SerializeField] Transform[] oldMirrors;
-    [SerializeField] LayerMask ignoreLayer;
+    [SerializeField] LineRenderer[] myLineRenderer;
+    [SerializeField] GameObject[] children;
     
     void Awake()
     {
-        myLineRenderer = GetComponent<LineRenderer>();
+        myLineRenderer = new LineRenderer[reflections];
+        children = new GameObject[reflections];
+        myLineRenderer[0] = GetComponent<LineRenderer>();
     }
 
     void Update()
     {
-        ray = new Ray(transform.position, transform.up);
-        
-        myLineRenderer.positionCount = 1;
-        myLineRenderer.SetPosition (0,transform.position);
-        float remainingRayLength = maxRayLength;
+        for (int i = 0; i < children.Length; i++)
+        {
+            Destroy(children[i]);
+        }
 
-        for (int i = 0; i < reflections; i++)
+        if (isActive)
         {
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, remainingRayLength, ignoreLayer);
-            if (hit)
+            ray = new Ray(transform.position, transform.up);
+            float remainingRayLength = maxRayLength;
+
+            for (int i = 0; i < reflections; i++)
             {
-                myLineRenderer.positionCount++;
-                myLineRenderer.SetPosition (myLineRenderer.positionCount - 1, hit.point);
-                remainingRayLength -= Vector3.Distance(ray.origin, hit.point);
-                ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
-                if (hit.collider.CompareTag("Mirror") == false)
+                children[i] = new GameObject("Line " + (i + 1), typeof(LineRenderer));
+                children[i].GetComponent<LineRenderer>().material = myMaterial;
+                children[i].GetComponent<LineRenderer>().textureMode = LineTextureMode.Tile;
+                children[i].GetComponent<LineRenderer>().sortingOrder = -50 + i;
+                children[i].transform.parent = transform;
+                myLineRenderer[i] = children[i].GetComponent<LineRenderer>();
+                RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, remainingRayLength, ignoreLayer);
+                if (hit)
                 {
-                    break;
+                    myLineRenderer[i].positionCount = 2;
+                    myLineRenderer[i].SetPosition(0, ray.origin);
+                    myLineRenderer[i].SetPosition(1, hit.point);
+                    remainingRayLength -= Vector3.Distance(ray.origin, hit.point);
+                    ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+                    if (hit.collider.CompareTag("Mirror") == false)
+                    {
+                        break;
+                    }
+                    hit.transform.gameObject.layer = 10;
+                    newMirrors = new Transform[oldMirrors.Length + 1];
+                    for (int f = 0; f < newMirrors.Length - 1; f++)
+                    {
+                        newMirrors[f] = oldMirrors[f];
+                        newMirrors[f].gameObject.layer = 0;
+                        newMirrors[f].tag = "Untagged";
+                    }
+                    newMirrors[newMirrors.Length - 1] = hit.transform;
+                    oldMirrors = newMirrors;
                 }
-                hit.transform.gameObject.layer = 10;
-                newMirrors = new Transform[oldMirrors.Length + 1];
-                for (int f = 0; f < newMirrors.Length - 1; f++)
+                else
                 {
-                    newMirrors[f] = oldMirrors[f];
-                    newMirrors[f].gameObject.layer = 0;
-                    newMirrors[f].tag = "Untagged";
+                    myLineRenderer[i].positionCount = 2;
+                    myLineRenderer[i].SetPosition(0, ray.origin);
+                    myLineRenderer[i].SetPosition(1, ray.origin + ray.direction * remainingRayLength);
                 }
-                newMirrors[newMirrors.Length - 1] = hit.transform;
-                oldMirrors = newMirrors;
             }
-            else
+            for (int i = 0; i < newMirrors.Length; i++)
             {
-                myLineRenderer.positionCount++;
-                myLineRenderer.SetPosition(myLineRenderer.positionCount - 1, ray.origin + ray.direction * remainingRayLength);
+                newMirrors[i].gameObject.layer = 0;
+                newMirrors[i].tag = "Mirror";
             }
+            newMirrors = new Transform[0];
+            oldMirrors = new Transform[0];
         }
-        for (int i = 0; i < newMirrors.Length; i++)
-        {
-            newMirrors[i].gameObject.layer = 0;
-            newMirrors[i].tag = "Mirror";
-        }
-        newMirrors = new Transform[0];
-        oldMirrors = new Transform[0];
     }
 }
