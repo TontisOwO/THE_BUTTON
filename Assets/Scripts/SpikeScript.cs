eae8a105c6d134b8b7eb711d32a0d352b8858503
Adonai <adonai.osterberg@elev.ga.lbs.se>
2025-05-27 09:47:59 +0200

    Spike, graphics workspace

--- Assets/Scripts/SpikeScript.cs
+++ Assets/Scripts/SpikeScript.cs
@@ -1,11 +1,15 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpikeScript : MonoBehaviour
 {
     [SerializeField] int damage = 1;
-    void Start()
+    [SerializeField] bool cooldownActive = false;
+
+    [SerializeField] Movement player;
+    void Awake()
     {
-        
+        player = GameObject.Find("Player").GetComponent<Movement>();
     }
 
     
@@ -16,9 +20,20 @@ public class SpikeScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && !cooldownActive)
         {
             //deal damage to the player equal to the damage value
+            player.HP -= damage;
+            StartCoroutine(DamageCooldown(0.25f));
+
         }
+    }
+
+    IEnumerator DamageCooldown(float duration)
+    {
+        cooldownActive = true;
+        yield return new WaitForSeconds(duration);
+        cooldownActive = false;
+        yield return null;
     }
 }
