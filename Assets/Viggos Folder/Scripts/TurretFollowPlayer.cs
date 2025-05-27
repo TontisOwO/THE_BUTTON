549cff2f981eecbcc49c36cc35f80733ce78db82
Viggo08 <viggo.wangeras@gmail.com>
2025-05-27 14:57:17 +0200

    Follow player turret
    Made a turret that follows the players transform when it's inside the detection range. Started with a still turret that only shoots forward

--- Assets/Viggos Folder/Scripts/TurretFollowPlayer.cs
+++ Assets/Viggos Folder/Scripts/TurretFollowPlayer.cs
@@ -1,18 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
 public class TurretFollowPlayer : MonoBehaviour
 {
-    //[SerializeField] TurretFollow;
+    public float DetectionRange;
+    public float FireRate;
+    public float Force;
+    public Transform PlayerTransform;
+    public GameObject TurretHead;
+    public GameObject Bullet;
+    public GameObject AlarmLight;
+    public Transform Shootpoint;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public bool playerDetected;
+
+    Vector2 Direction;
+
+    float nextTimeToFire = 0;
+
+    private void FixedUpdate()
     {
-        
+        Vector2 targetPos = PlayerTransform.position;
+        Direction = targetPos - (Vector2)transform.position;
+
+        DetectPlayer();
+
+        if (playerDetected)
+        {
+            AlarmLight.GetComponent<SpriteRenderer>().color = Color.red;
+            Vector3 Look = transform.InverseTransformPoint(PlayerTransform.transform.position);
+            float Angle = Mathf.Atan2(Look.y, Look.x) * Mathf.Rad2Deg + 270;
+            transform.Rotate(0, 0, Angle);
+
+            if (Time.time > nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1 / FireRate;
+                shoot();
+
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void shoot()
     {
-        
+        GameObject newBullet = Instantiate(Bullet, Shootpoint.position, Quaternion.identity);
+        newBullet.GetComponent<Rigidbody2D>().AddForce(Direction * Force, ForceMode2D.Force);
+    }
+
+    private void DetectPlayer()
+    {
+        //is player within sight range?
+        if (Vector2.Distance(transform.position, PlayerTransform.position) <= DetectionRange)
+        {
+            playerDetected = true;
+        }
+        else
+        {
+            playerDetected = false;
+            AlarmLight.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, DetectionRange);
     }
 }
