708903527978343e2d0a6cfecca1b4ee3894d640
Adonai <adonai.osterberg@elev.ga.lbs.se>
2025-05-27 13:55:08 +0200

    working on save data

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -11,16 +11,22 @@ public class SceneLoader : MonoBehaviour
     [SerializeField] ScreenFade myFader;
 
     [SerializeField] string sceneName;
+    [SerializeField] public int currentLevel = 1;
+
+    public static SceneLoader Instance;
 
     void Awake()
     {
-        
+        if (Instance = null)
+        {
+            Instance = this;
+        }
     }
 
 
     void Update()
     {
-
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -36,6 +42,11 @@ public class SceneLoader : MonoBehaviour
         StartCoroutine(StartLoadScene(loadSceneName));
     }
 
+    public void StartGame()
+    {
+        
+    }
+
     public void QuitGame()
     {
         Application.Quit();
@@ -49,4 +60,8 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(sceneToLoad);
     }
 
+    private void OnApplicationQuit( )
+    {
+        SaveData.SaveGameState();
+    }
 }
